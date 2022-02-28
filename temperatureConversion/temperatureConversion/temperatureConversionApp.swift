ce01b590307c1a2cbad8dfe553189c3949ce0eb4
Frank Chu <yongfrank@outlook.com>
2022-02-28 12:02:14 +0800

    create temperature conversion and add screenshot to WeSplit

---
+++ temperatureConversion/temperatureConversion/temperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  temperatureConversionApp.swift
+//  temperatureConversion
+//
+//  Created by Frank Chu on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct temperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
