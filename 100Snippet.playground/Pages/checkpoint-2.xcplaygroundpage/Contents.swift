7140112dd248eb198734773225ecbcb3f0db7d01
Frank Chu <yongfrank@outlook.com>
2022-02-28 10:36:44 +0800

    checkPoint-2

---
+++ 100Snippet.playground/Pages/checkpoint-2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var arrays = ["Facebook", "LinkedIn", "Apple", "Google", "Apple"]
+print(arrays.count)
+
+var setOfArrarys = Set(arrays)
+print(setOfArrarys.count)
+
+//: [Next](@next)
