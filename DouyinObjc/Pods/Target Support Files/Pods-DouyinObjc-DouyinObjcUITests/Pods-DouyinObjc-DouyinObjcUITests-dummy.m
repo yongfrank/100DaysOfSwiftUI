f4a75bdd09d61f42f9ee23d16a3106d86ea9f801
Chu Yong <chuyong155@hellobike.com>
2023-05-13 10:49:21 +0800

    update .gitignore

---
+++ DouyinObjc/Pods/Target Support Files/Pods-DouyinObjc-DouyinObjcUITests/Pods-DouyinObjc-DouyinObjcUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DouyinObjc_DouyinObjcUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DouyinObjc_DouyinObjcUITests
+@end
---
+++ DouyinObjc/Pods/Target Support Files/Pods-DouyinObjcTests/Pods-DouyinObjcTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DouyinObjcTests : NSObject
+@end
+@implementation PodsDummy_Pods_DouyinObjcTests
+@end
